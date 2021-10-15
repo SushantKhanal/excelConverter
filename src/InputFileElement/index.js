3eb13532830f956c2956a3045e6b45e56a0b5295
SushantKhanal <dreamerking114@gmail.com>
2021-10-14 19:34:14 -0500

    cleaning code

--- src/InputFileElement/index.js
+++ src/InputFileElement/index.js
@@ -30,24 +30,18 @@ const InputElement = (props) => {
     }, []);
 
     const compareTwoNames = (nameA, nameB) => {
-        nameA = nameA.trim();
-        nameB = nameB.trim();
-        const nameAInitialLetter = nameA.length > 0 ? nameA[0] : '';
-        const nameBInitialLetter = nameB.length > 0 ? nameB[0] : '';
-    
+
         if(nameA === nameB) return 1; //same words same arrangement
-    
         let nameAwords = nameA.split(" ").map(x => x.toUpperCase());
         let nameBwords = nameB.split(" ").map(x => x.toUpperCase());
-    
+
+        const wordCountDiff = Math.abs(nameAwords.length - nameBwords.length);
+        if(wordCountDiff >= 2) return 0; //false
+
         if(checkIfWordsHasAbbrEquality(nameAwords, nameBwords)) return 3;
         
         let setForNameA = new Set(nameAwords);
         let setForNameB = new Set(nameBwords);
-    
-        const wordCountDiff = Math.abs(nameAwords.length - nameBwords.length);
-    
-        if(wordCountDiff >= 2) return 0; //false
     
         if(nameAwords.length < nameBwords.length) {
           let res = switchNamesArrayAndSets(setForNameA, setForNameB, nameAwords, nameBwords);
@@ -63,7 +57,7 @@ const InputElement = (props) => {
     
         if(count === 0) return 2; //2 = same words different arrangement
     
-        if(isUnsure(count, nameAInitialLetter, nameBInitialLetter)) return '?';
+        if(isUnsure(count, nameA, nameB)) return '?';
         //match === '?', if only one word or letter is different in the names AND ...
         //...first letter in the two columns are the same (only for last condition)?
     }
@@ -73,7 +67,7 @@ const InputElement = (props) => {
         readXlsxFile(input.files[0]).then((rows) => {
             for(let i = 1; i < rows.length; i++) {
             const row = rows[i];
-            let match = compareTwoNames(row[getExcelCell['comnam']], row[getExcelCell['newvar']]);
+            let match = compareTwoNames(row[getExcelCell['comnam']].trim(), row[getExcelCell['newvar']].trim());
             if(match === 1) row[getExcelCell['matched']] = 1;
             if(match === 2) row[getExcelCell['matched']] = 2;
             if(match === 3) row[getExcelCell['matched']] = 3;
@@ -105,7 +99,9 @@ const InputElement = (props) => {
         return count;
       }
     
-      const isUnsure = (count, nameAInitialLetter, nameBInitialLetter) => {
+      const isUnsure = (count, nameA, nameB) => {
+        const nameAInitialLetter = nameA.length > 0 ? nameA[0] : '';
+        const nameBInitialLetter = nameB.length > 0 ? nameB[0] : '';
         if(count === 1) {
           if(nameAInitialLetter === nameBInitialLetter) {
             return true; 
